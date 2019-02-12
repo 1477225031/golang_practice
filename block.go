716f806f83f4c6ad0fe896bd6b268287a24fba13
xuelei <1477225031@qq.com>
2019-02-12 16:33:39 +0800

    修改包名

--- block.go
+++ block.go
@@ -3,15 +3,15 @@ package demochain
 import (
 	"crypto/sha256"
 	"encoding/hex"
+	"time"
 )
 
-//create a project
 //区块链中的区块,以结构体形式展开
-
 type Block struct {
 	Index int64  //区块编码
+	Timestamp int64  //创建时间戳
+	PreBlockHash string //钱一个
 	Hash string  //该区块的hash值
-	PreHash string //钱一个
 	Data string    //区块信息
 }
 
@@ -19,10 +19,35 @@ type Block struct {
 //返回加密字符串
 func calculateHash(block Block) string{
     //除了hash
-    string := string(block.Index) + string(block.PreHash) + block.Data
+    string := string(block.Index) + string(block.PreBlockHash) + block.Data
     //进行加密
     hashInBytes := sha256.Sum256([]byte(string))
     //使用hex十六进制进行解密
     hashInstring :=hex.EncodeToString(hashInBytes[:])
     return hashInstring
+}
+
+//添加区块  该区块依赖上一个区块
+func GenerateNewBlock(preBlock Block,data string) Block {
+    //index
+    newBlock := Block{}
+	newBlock.Index = preBlock.Index + 1
+	newBlock.PreBlockHash = preBlock.Hash
+	newBlock.Timestamp = time.Now().Unix()
+	newBlock.Data = data
+	newBlock.Hash = calculateHash(newBlock)
+    return newBlock
+}
+
+
+
+//创始区块 初始区块链不用返回数据
+func GenerateGenisisBlock() Block{
+	preBlock := Block{}
+	preBlock.Index = -1
+	preBlock.Timestamp = time.Now().Unix()
+	preBlock.PreBlockHash = ""
+	preBlock.Hash = calculateHash(preBlock)
+	//生成区块
+	return GenerateNewBlock(preBlock, "Genesis")
 }
--- blockchain.go
+++ blockchain.go
@@ -1,1 +1,15 @@
-package main
+package demochain
+
+import "fmt"
+
+//处理区块链
+//无法删除,无法修改
+//可以查看,可以新增
+//定义数据的默认结构
+type BlockChain struct {
+	Blocks []*Block
+}
+
+func main()  {
+	fmt.Println(BlockChain{})
+}
